f2a92cb2ca835795e5452ca3e431192b25fe708a
teachernyok1207 <104883084+teachernyok1207@users.noreply.github.com>
2023-08-29 01:26:08 +0800

    Remove Fake Logout

--- components/Header.jsx
+++ components/Header.jsx
@@ -41,7 +41,6 @@ export default function Header() {
                         className="login-icon"
                     />
                 </Link>
-                <button onClick={fakeLogOut}>X</button>
             </nav>
         </header>
     )
